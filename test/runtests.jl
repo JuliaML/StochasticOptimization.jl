e97d7c77ff94401b25265ce66dca6b828e217aa7
Thomas Breloff <tom@breloff.com>
2016-09-09 12:12:33 -0400

    linreg test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,40 +10,48 @@ using Plots; unicodeplots(show=true)
 @testset "LinReg" begin
 
     let nin=10, nout=1
+        # build our objective
         t = Affine{Float64}(nin,nout)
         l = L2DistLoss()
         p = L1Penalty(1e-8)
         obj = RegularizedObjective(t, l, p)
 
-        τ = 10000
+        # create some fake data... affine transform plus noise
+        τ = 1000
         w = randn(nout, nin)
         b = randn(nout)
         inputs = randn(nin, τ)
         targets = w * inputs + repmat(b, 1, τ) + 0.1randn(nout, τ)
 
+        # our learning strategy... SGD with a fixed learning rate
         strat = GradientDescent(FixedLR(5e-3), SGD())
         @show strat
 
-        data = batches(inputs, targets; batch_size = 100)
-        @show length(data)
+        # trace setup
         tr = MVHistory()
-        outputs = zeros(0)
         normw = normb = 0.0
-        for i=1:1000
+
+        # do a bunch of epochs and trace/show in between
+        for i=1:100
+            # get new random batches, then learn from them
+            data = batches(inputs, targets; batch_size = 100)
             learn!(obj, strat, data)
+
+            # trace and show
+            # TODO: make this a LearningStrategy which implements post_hook
             normw = norm(value(t.w) - w)
             normb = norm(value(t.b) - b)
             @trace tr i normw normb
-            @show i normw normb
+            @show i, normw, normb
         end
-        # @test value(t.w) ≈ w
-        # @test value(t.b) ≈ b
 
+        # show the trace we collected
         plot(tr)
 
+        # scatter predicted output vs ground truth... should be diagonal line
         pred = value(t.w) * inputs + repmat(value(t.b), 1, τ)
         truth = w * inputs + repmat(b, 1, τ)
-        @test norm(pred - truth) < 1e-4
+        @test maximum(pred - truth) < 1e-1
         plot(pred', truth', t=:scatter)
     end
 
