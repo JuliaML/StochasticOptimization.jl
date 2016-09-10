642d0fc4ba948aa3e293d6c890e18e4f51ed2b4d
Thomas Breloff <tom@breloff.com>
2016-09-09 22:51:12 -0400

    Adagrad

--- src/paramupdaters.jl
+++ src/paramupdaters.jl
@@ -1,4 +1,7 @@
 
+# TODO: add citations/links for each method
+
+"Stochastic Gradient Descent with Momentum"
 type SGD{T<:Number} <: ParamUpdater
     mom::T # momentum
     last_Δ::Vector{T}
@@ -21,6 +24,27 @@ function update!(θ::AbstractVector, updater::SGD, ∇::AbstractVector, lr::Numb
 end
 
 # -----------------------------------------------------------------------
+
+"Adaptive Gradient"
+type Adagrad{T<:Number} <: ParamUpdater
+    ϵ::T
+    G::Vector{T}  # sum of squared gradients
+    Adagrad(ϵ::T) = new(ϵ)
+end
+Adagrad{T}(::Type{T}, ϵ = T(0.01)) = Adagrad{T}(T(ϵ))
+Adagrad{T}(ϵ::T = 0.01) = Adagrad{T}(ϵ)
+
+function init(updater::Adagrad, model)
+    updater.G = zeros(params(model))
+    return
+end
+
+function update!(θ::AbstractVector, updater::Adagrad, ∇::AbstractVector, lr::Number)
+    for (i,j,k) in zip(eachindex(θ), eachindex(∇), eachindex(updater.G))
+        updater.G[k] += ∇[j]^2
+        Θ[i] -= lr * ∇[j] / sqrt(updater.ϵ + updater.G[k])
+    end
+end
 
 # -----------------------------------------------------------------------
 
