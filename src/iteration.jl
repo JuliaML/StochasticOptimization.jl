6b1b1ab3670428d53f1bfe7ad7bca62fab5aedb2
Christof Stocker <c.stocker@hci-kdd.org>
2016-10-20 13:43:57 -0400

    compute_batch_settings

--- src/iteration.jl
+++ src/iteration.jl
@@ -143,6 +143,11 @@ getobs(tup::Tuple{}) = ()
 
 obstype(source) = typeof(getobs(source, 1))
 
+
+# ----------------------------------------------------------------------------
+# ObsIterator
+# ----------------------------------------------------------------------------
+
 # default implementations
 nobs(itr::ObsIterator) = nobs(itr.source)
 all_indices(itr::ObsIterator) = 1:nobs(itr)
@@ -154,7 +159,6 @@ Base.length(itr::ObsIterator) = length(all_indices(itr))
 Base.size(itr::ObsIterator) = size(all_indices(itr))
 Base.getindex(itr::ObsIterator, idx) = getobs(itr, idx)
 Base.get(itr::ObsIterator) = collect(itr)
-
 
 # ----------------------------------------------------------------------------
 
@@ -254,7 +258,45 @@ filtered_obs(f::Function, source) = SubsetObs(source, filter(f, 1:nobs(source)))
 filtered_obs(f::Function, s_1, s_2, s_rest...) = filtered_obs(f, (s_1, s_2, s_rest...))
 
 # ----------------------------------------------------------------------------
+# BatchIterator
+# ----------------------------------------------------------------------------
 
+default_batch_size(source) = clamp(div(nobs(source), 5), 1, 100)
+
+"""
+Helper function to compute sensible and compatible values for the
+`size` and `count`
+"""
+function _compute_batch_settings(source, size::Int = -1, count::Int = -1)
+    num_observations = nobs(source)::Int
+    @assert num_observations > 0
+    size  <= num_observations || throw(BoundsError(source,size))
+    count <= num_observations || throw(BoundsError(source,count))
+    if size <= 0 && count <= 0
+        # no batch settings specified, use default size and as many batches as possible
+        size = default_batch_size(source)::Int
+        count = floor(Int, num_observations / size)
+    elseif size <= 0
+        # use count to determine size. uses all observations
+        size = floor(Int, num_observations / count)
+    elseif count <= 0
+        # use size and as many batches as possible
+        count = floor(Int, num_observations / size)
+    else
+        # try to use both (usually to use a subset of the observations)
+        max_batchcount = floor(Int, num_observations / size)
+        count <= max_batchcount || throw(DimensionMismatch("Specified number of partitions is not possible with specified size"))
+    end
+
+    # check if the settings will result in all data points being used
+    unused = num_observations % size
+    if unused > 0
+        info("The specified values for size and/or count will result in $unused unused data points")
+    end
+    size::Int, count::Int
+end
+
+# ----------------------------------------------------------------------------
 
 immutable EachBatch{S,T} <: BatchIterator{T}
     source::S
